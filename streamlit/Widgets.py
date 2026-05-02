67ade9ea35d13db7096988fd2bd8d339f872ff59
CHARAN@2027 <131586584+BANDARU-SAICHARAN@users.noreply.github.com>
2026-05-02 11:46:16 +0530

    Update Widgets.py

--- streamlit/Widgets.py
+++ streamlit/Widgets.py
@@ -8,7 +8,7 @@ name = st.text_input("Enter your Name :")
 if name:
     st.write(f"Hello, {name}")
 
-age = st.slider("Select your age :", 0, 100, 25)
+age = st.slider("Select your age :", 0, 100,25)
 st.write(f"Your age is {age}")
 
 data = {
